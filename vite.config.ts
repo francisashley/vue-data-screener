6a30d353ca55e2546ac5dee0889de3060921f788
skystash <fa.repo.dev@gmail.com>
2022-07-19 19:27:39 +0100

    Switch from vue-cli-service to vite

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from "vite";
+import { createVuePlugin as vue } from "vite-plugin-vue2";
+import { visualizer } from "rollup-plugin-visualizer";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const plugins = [vue()];
+
+  if (mode === "report") {
+    plugins.push(
+      visualizer({
+        filename: "./dist/report.html",
+        open: true,
+        brotliSize: true,
+      })
+    );
+  }
+
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, "src/index.ts"),
+        name: "VueDataScreener",
+        fileName: "index",
+      },
+      rollupOptions: {
+        external: ["vue"],
+        output: {
+          globals: {
+            vue: "Vue",
+          },
+        },
+      },
+    },
+  };
+});
